01423479ebcba8756f2544a2b9d6679e473795b6
Satwik Kini <satwikkini01@gmail.com>
2023-10-03 20:23:59 +0530

    Updates

---
+++ precache-manifest.e721d0add1b14722dc2a38bd65de8359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "299c1d75da2ffbd6033c",
+    "url": "/text_utils%60/static/css/main.d30c7fe3.chunk.css"
+  },
+  {
+    "revision": "299c1d75da2ffbd6033c",
+    "url": "/text_utils%60/static/js/main.299c1d75.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/text_utils%60/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/text_utils%60/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "65a793974b03b11cf9c6",
+    "url": "/text_utils%60/static/js/runtime~main.65a79397.js"
+  },
+  {
+    "revision": "cbf65c5f8236d44c97e003541151c70d",
+    "url": "/text_utils%60/index.html"
+  }
+];
